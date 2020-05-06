4d053ba51009b3e74d90b6a08fab9bdd15168554
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-05-06 22:24:32 +0300

    hw3 improved version

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -1,38 +1,32 @@
-var el = document.body;
-var btn = document.getElementsByClassName('l-tasks__btn_toggle');
-
+var body = document.body;
+var btnToggle = document.getElementsByClassName('l-tasks__btn _toggle');
+var btnRemove = document.getElementsByClassName('l-tasks__btn _remove');
+var spansArray = document.getElementsByClassName('l-tasks__span');
 var clickHandler = function(){
-  el.classList.toggle('_color-schemie-light');
+  body.classList.toggle('_color-schemie-light');
 }
-btn[0].addEventListener('click',clickHandler);
+  btnToggle[0].addEventListener('click',clickHandler);
 
-var btn1 = document.getElementsByClassName('l-tasks__btn_remove');
-var sp = document.getElementsByClassName('l-tasks__span');
-var removeSp = function(){
-  // for (var i = 0; i < classList.length; i++){
-     if(document.body.classList.contains('_color-schemie-light')){
-
-      sp[sp.length-1].remove('l-tasks__span'); 
-      btn1[0].removeEventListener('click',removeSp);
+var removeSpanHandler = function(){
+     if(body.classList.contains('_color-schemie-light')){
+      spansArray[spansArray.length-1].remove('l-tasks__span'); 
+      btnRemove[0].removeEventListener('click',removeSpanHandler);
      }
      else{
-      sp[0].remove('l-tasks__span');}
-      btn1[0].removeEventListener('click',removeSp);
+      spansArray[0].remove('l-tasks__span');}
+      btnRemove[0].removeEventListener('click',removeSpanHandler);
     }
-
-btn1[0].addEventListener('click',removeSp);
-
-var result = (function(num) {
-  
-  if( typeof num ==='number' && num % 2==0){
-    document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is a number:'+' ' + num +' '+'even';
+  btnRemove[0].addEventListener('click',removeSpanHandler);
+;(function(num) {
+  if( typeof num ==='number' && num % 2===0){
+    document.getElementsByClassName('l-tasks__item-text')[0].textContent ='Number'+' '+ num +' '+'is'+' '+'even';
   }
   else
     if(typeof num ==='number' && num % 2!==0){
-     document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is a number:'+' ' + num +' '+'odd';
+     document.getElementsByClassName('l-tasks__item-text')[0].textContent ='Number'+' '+ num +' '+'is'+' '+'odd';
   } 
     else {
      document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is not a number';
    };
-  }(9));
+  }(1));
   
