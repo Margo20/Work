b2630795b4ee9f7d31aec49f8f1c06dddd6ceff0
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-05-09 18:15:03 +0300

    improved oddEven and css

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -2,33 +2,37 @@ var body = document.body;
 var btnToggle = document.getElementsByClassName('l-tasks__btn _toggle')[0];
 var btnRemove = document.getElementsByClassName('l-tasks__btn _remove')[0];
 var spansArray = document.getElementsByClassName('l-tasks__span');
+var itemText = document.getElementsByClassName('l-tasks__item-text')[0];
 var changeOfSubjectHandler = function(){
   body.classList.toggle('_color-schemie-light');
 }
 var removeSpanHandler = function(){
      if(body.classList.contains('_color-schemie-light')){
       spansArray[spansArray.length-1].remove(); 
-      btnRemove.removeEventListener('click',removeSpanHandler);
-     }
-     else{
+     } else{
       spansArray[0].remove();
     }
+
       btnRemove.removeEventListener('click',removeSpanHandler);
     }
 
   btnRemove.addEventListener('click',removeSpanHandler);
   btnToggle.addEventListener('click',changeOfSubjectHandler);
 
-;(function(num) {
-  if( typeof num ==='number' && num % 2===0){
-    document.getElementsByClassName('l-tasks__item-text')[0].textContent ='Number'+' '+ num +' '+'is'+' '+'even';
-  }
-  else
-    if(typeof num ==='number' && num % 2!==0){
-     document.getElementsByClassName('l-tasks__item-text')[0].textContent ='Number'+' '+ num +' '+'is'+' '+'odd';
-  } 
-    else {
-     document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is not a number';
-   };
-  }(12));
-  
+    ;(function(num) {
+    var type = 'even';
+
+      if( typeof num !=='number'){
+         
+        return itemText.textContent ='This is not a number';
+      }
+
+      if(num % 2!==0){
+        type = 'odd';
+      } 
+        
+      itemText.textContent ='Number '+ num +' is '+ type;
+       
+      }(1));
+
+
