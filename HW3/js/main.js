ab2ce9f52bb0bd18878d2546bbc46ae9669b8dc5
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-05-10 21:41:28 +0300

    formatting hw3

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -3,36 +3,32 @@ var btnToggle = document.getElementsByClassName('l-tasks__btn _toggle')[0];
 var btnRemove = document.getElementsByClassName('l-tasks__btn _remove')[0];
 var spansArray = document.getElementsByClassName('l-tasks__span');
 var itemText = document.getElementsByClassName('l-tasks__item-text')[0];
-var changeOfSubjectHandler = function(){
+var changeOfSubjectHandler = function() {
   body.classList.toggle('_color-schemie-light');
 }
-var removeSpanHandler = function(){
-     if(body.classList.contains('_color-schemie-light')){
-      spansArray[spansArray.length-1].remove(); 
-     } else{
-      spansArray[0].remove();
-    }
+var removeSpanHandler = function() {
+  if (body.classList.contains('_color-schemie-light')) {
+    spansArray[spansArray.length-1].remove(); 
+  } else {
+    spansArray[0].remove();
+  }
+  btnRemove.removeEventListener('click', removeSpanHandler);
+}
 
-      btnRemove.removeEventListener('click',removeSpanHandler);
-    }
+btnRemove.addEventListener('click', removeSpanHandler);
+btnToggle.addEventListener('click', changeOfSubjectHandler);
 
-  btnRemove.addEventListener('click',removeSpanHandler);
-  btnToggle.addEventListener('click',changeOfSubjectHandler);
+(function(num) {
+  var type = 'even';
+  if (typeof num !== 'number') {
 
-    ;(function(num) {
-    var type = 'even';
+    return itemText.textContent = 'This is not a number';
+  }
 
-      if( typeof num !=='number'){
-         
-        return itemText.textContent ='This is not a number';
-      }
-
-      if(num % 2!==0){
-        type = 'odd';
-      } 
-        
-      itemText.textContent ='Number '+ num +' is '+ type;
-       
-      }(1));
+  if (num % 2 !== 0) {
+    type = 'odd';
+  }
+  itemText.textContent = 'Number ' + num + ' is ' + type;
+}(1));
 
 
