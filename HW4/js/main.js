7d7670a8e5eb2bc5b0c9f9d57d908313e8a3e1aa
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-05-13 16:34:15 +0300

    add transition

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -1,32 +1,33 @@
-  var univ = document.querySelector('.b-universe');
-  var earth = document.querySelector('.b-universe__img');
-  var earthWidth = earth.width / 2;
-  var earthHeight = earth.height / 2;
-  univ.addEventListener('click', function(e) {
-   if (e.target.tagName === 'DIV' && earth.style.display !== 'none') {
-      var x = e.offsetX;
-      var y = e.offsetY;
-      if (y >= univ.offsetHeight - earthHeight) {
-        y = univ.offsetHeight - earthHeight;
-      }
-      if (y < earthHeight) {
-        y = earthHeight;
-      }
-      if (x >= univ.offsetWidth - earthWidth) {
-        x = univ.offsetWidth - earthWidth;
-      }
-      if (x < earthWidth) {
-        x = earthWidth;
-      }
-        earth.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-    }
+var univ = document.querySelector('.b-universe');
+var earth = document.querySelector('.b-universe__img');
+var earthWidth = earth.width /2 ;
+var earthHeight = earth.height /2 ;
 
-    if (e.target.tagName === 'IMG') {
-      earth.style.display = 'none';
-    } else if (e.target.tagName === 'DIV'){
-      earth.style.display = '';
+univ.addEventListener('click', function(e) {
+ if (e.target.tagName === 'DIV' && earth.style.display !== 'none') {
+    var x = e.offsetX;
+    var y = e.offsetY;
 
-     return; 
+    if (y >= univ.offsetHeight - earthHeight) {
+      y = univ.offsetHeight - earthHeight;
     }
-  });
+    if (y < earthHeight) {
+      y = earthHeight;
+    }
+    if (x >= univ.offsetWidth - earthWidth) {
+      x = univ.offsetWidth - earthWidth;
+    }
+    if (x < earthWidth) {
+      x = earthWidth;
+    }
+      earth.style.transform = 'translate(' + (x - 50) + 'px, ' + (y - 50) + 'px)';
+  }
+  if (e.target.tagName === 'IMG') {
+    earth.style.display = 'none';
+  } else if (e.target.tagName === 'DIV'){
+    earth.style.display = '';
+
+   return; 
+  }
+});
 
