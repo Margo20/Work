6cedb83107dfe6a4563a9b719728a3d8063fbfa3
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-05-20 22:12:56 +0300

    second option

--- HW6/js/main.js
+++ HW6/js/main.js
@@ -1,6 +1,7 @@
 var body = document.body;
-var butt = document.querySelector('.btn');
+var btn = document.querySelector('.btn');
 var colors = [];
+var num;
 var obj = {
     field1: 'one',
     field2: 'two',
@@ -8,32 +9,41 @@ var obj = {
     field4: 'four',
     field5: ['#00bcd4', ' #ffc107', '#2196f3']
   };
-alert('Достаньте массив цветов');
 
-butt.addEventListener('click', function(e) {
-  lengthColors = colors.length;
-  num = Math.floor(Math.random() * lengthColors);
-  body.style.backgroundColor = colors[num];
-  if (colors.length == 0) {
-    const keys = Object.keys(obj);
-      for (const key of keys) {
-        const someObject = obj[key];
-        if (Array.isArray(someObject)) {
-          colors = someObject;      
-        }
+btn.addEventListener('click', function() {
+  if (!colors.length) {
+    var fieldArray = Object.getOwnPropertyNames(obj);
+
+    for (i = 0; i < fieldArray.length; i++) {
+      var field = fieldArray[i];
+      var someObject = obj[field];
+
+      if (Array.isArray(someObject)) {
+         colors = someObject;
       }
+    }
   }
+  num = Math.floor(Math.random() * colors.length);
+  body.style.backgroundColor = colors[num];
 });
 
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'ArrowRight') {
-    num = num + 1;
+document.addEventListener('keydown', function (event) {
+  var isLeftKey = event.code === 'ArrowLeft' ;
+  var isRightKey = event.code === 'ArrowRight';
+
+  if ((isRightKey || isLeftKey) && !colors.length) {
+    alert('Достаньте массив цветов');
+    return;
+  }
+
+  if (isRightKey) {
+    num++;
     if (num >= colors.length) {
       num = 0;
     }
     body.style.backgroundColor = colors[num];
-  } else if (event.code == 'ArrowLeft') {
-    num = num - 1;
+  } else if (isLeftKey) {
+    num--;
     if (num < 0) {
       num = colors.length - 1;
     }
