e130f89f0dca28940ceae9f3628894970c7c7716
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-05-27 17:41:24 +0300

    first part changes

--- additionalHW/source/js/main.js
+++ additionalHW/source/js/main.js
@@ -1,58 +1,41 @@
-var aim = document.querySelector('.b-shooter__aim');
-var imgAim = document.querySelector('.b-shooter__img-aim');
-var cemetery = document.querySelector('.b-shooter');
-var aimHalfWidth = imgAim.width / 2 ;
-var aimHalfHeight = imgAim.height / 2 ;
-var calcY = cemetery.offsetHeight - imgAim.height;
-var calcX = cemetery.offsetWidth - imgAim.width;
+  var aim = document.querySelector('.b-shooter__aim');
+  var imgAim = document.querySelector('.b-shooter__img-aim');
+  var cemetery = document.querySelector('.b-shooter');
+  var aimHalfWidth = imgAim.width / 2 ;
+  var aimHalfHeight = imgAim.height / 2 ;
 
+  cemetery.addEventListener('click', function(e) {
+    var calcY = e.currentTarget.offsetHeight - aim.offsetHeight;
+    var calcX = e.currentTarget.offsetWidth - aim.offsetWidth;
+    var x = e.offsetX - aimHalfWidth;
+    var y = e.offsetY - aimHalfHeight;
 
-cemetery.addEventListener('click', function(e) {
-  var x = e.offsetX - aimHalfWidth;
-  var y = e.offsetY - aimHalfHeight;
+    if (y >= calcY) {
+      y = calcY;
+    } else if (y < 0) {
+      y = 0;
+    }
 
-  if (y >= calcY) {
-    y = calcY;
-  } else if (y < 0) {
-    y = 0;
-  }
+    if (x >= calcX) {
+      x = calcX;
+    } else if (x < 0) {
+      x = 0;
+    }
+    aim.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  });
 
-  if (x >= calcX) {
-    x = calcX;
-  } else if (x < 0) {
-    x = 0;
-  }
+  document.addEventListener('keydown', function(e) {
+    var isEnterKey = e.code === 'Enter';
 
-  imgAim.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-});
-document.addEventListener('keydown', function(e) {
-  var isEnterKey = e.code === 'Enter';
-  if (isEnterKey) {
-    imgAim.style.padding = '10%';
-  }
-});
+    if (isEnterKey) {
+      imgAim.style.transform = 'scale(0.9)';
+    }
+  });
+
   document.addEventListener('keyup', function(e) {
     var isEnterKey = e.code === 'Enter';
+
     if (isEnterKey) {
-      imgAim.style.padding = '0';
+      imgAim.style.transform = 'scale(1)';
     };
-
-});
-
-// document.addEventListener('keydown', function(e) {
-//   var isEnterKey = e.code === 'Enter';
-//   if (isEnterKey) {
-//     imgAim.style.padding = '10%';
-//   }
-// });
-//   document.addEventListener('keyup', function(e) {
-//     var isEnterKey = e.code === 'Enter';
-//     if (isEnterKey) {
-//       imgAim.style.padding = '0';
-//     };
-//
-// });
-
-
-
-
+  });
