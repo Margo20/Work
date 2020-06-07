28e201652ee2849bfe0406981c6bdc52ae498d2b
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-06-07 23:06:10 +0300

    part3 changes

--- additionalHW/source/js/main.js
+++ additionalHW/source/js/main.js
@@ -2,14 +2,14 @@
   var imgAim = document.querySelector('.b-shooter__img-aim');
   var ghost = document.querySelector('.b-shooter__img-ghost');
   var cemetery = document.querySelector('.b-shooter');
-  var aimHalfWidth = imgAim.width / 2 ;
-  var aimHalfHeight = imgAim.height / 2 ;
+  var fire = document.querySelector('.b-shooter__img-fire');
+  var delayToReset = 500;
 
   cemetery.addEventListener('click', function(e) {
     var calcY = e.currentTarget.offsetHeight - aim.offsetHeight;
     var calcX = e.currentTarget.offsetWidth - aim.offsetWidth;
-    var x = e.offsetX - aimHalfWidth;
-    var y = e.offsetY - aimHalfHeight;
+    var y = e.offsetY - imgAim.height / 2 ;
+    var x = e.offsetX - imgAim.width / 2 ;
 
     if (y >= calcY) {
       y = calcY;
@@ -26,24 +26,43 @@
   });
 
   document.addEventListener('keydown', function(e) {
-    var isEnterKey = e.code === 'Enter';
-    var domRect = imgAim.getBoundingClientRect();
-    var aimCenterX = domRect.x / 2;
-    var aimCenterY = domRect.y / 2;
-
-    if (isEnterKey) {
+    if (e.code === 'Enter') {
       imgAim.style.transform = 'scale(0.9)';
     }
-
-    console.log(aimCenterX);
-    console.log(aimCenterY);
   });
 
   document.addEventListener('keyup', function(e) {
-    var isEnterKey = e.code === 'Enter';
+    if (e.code === 'Enter') {
+      var domRect = imgAim.getBoundingClientRect();
+      var domGhost = ghost.getBoundingClientRect();
+      var aimCenterX = domRect.width / 2 + domRect.x;
+      var aimCenterY = domRect.height / 2 + domRect.y;
 
-    if (isEnterKey) {
       imgAim.style.transform = '';
+      console.log(aimCenterX, aimCenterY);
+
+      if (
+        aimCenterX <= domGhost.right - 20
+        && aimCenterX >= domGhost.left + 20
+        && aimCenterY >= domGhost.top -20
+        && aimCenterY <= domGhost.bottom + 20
+      ) {
+        var animationGhost = `opacity: 0; transition-duration: ${delayToReset * 0.6}ms;
+                transition-delay: ${delayToReset * 0.4}ms;`;
+
+        ghost.style.cssText += animationGhost;
+        imgAim.style.display = 'none';
+        fire.style.cssText = `opacity: 0; transition-duration: ${delayToReset * 0.6}ms;
+                visibility: visible; transition-delay: ${delayToReset * 0.4}ms;`;
+
+        setTimeout(function() {
+          fire.removeAttribute('style');
+          fire.style.visibility = 'hidden';
+          imgAim.style.display = '';
+          ghost.removeAttribute('style');
+          ghost.style.display = 'none';
+        }, delayToReset);
+      };
     };
   });
 
@@ -52,21 +71,13 @@
     var limitY = cemetery.offsetHeight - ghost.offsetHeight;
     var x = Math.floor(Math.random() * (limitX + 1));
     var y = Math.floor(Math.random() * (limitY + 1));
-
-    ghost.style.cssText = 'transform: translate(' + x + 'px, ' + y + 'px);';
+    ghost.style.left = x + 'px';
+    ghost.style.top = y + 'px';
   };
 
-  if (ghost.style.display === 'none') {
-    ghost.style.display = ''; 
-  }
-
   setInterval(function () {
-
+    if (ghost.style.display === 'none') {
+      ghost.style.display = '';
+    }
     setRandomCoords();
   }, 3000);
-
-
-
-
-
-
