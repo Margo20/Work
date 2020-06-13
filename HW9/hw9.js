665ad2526f5803e8abb22f5028c3a32273c67cf2
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-06-13 09:02:15 +0300

    import

--- HW9/hw9.js
+++ HW9/hw9.js
@@ -1,86 +1,8 @@
 'use strict'
 
-const requestForCountries = function (cb) {
-  const countries = [
-    {
-      userId: 44,
-      country: 'Germany'
-    },
-    {
-      userId: 1,
-      country: 'Canada'
-    },
-    {
-      userId: 342,
-      country: 'Brazil'
-    },
-    {
-      userId: 24,
-      country: 'Denmark'
-    },
-    {
-      userId: 2,
-      country: 'Ireland'
-    }
-  ];
+import {requestForUsers, requestForCountries} from './main2.js';
 
-  setTimeout(function () {
-    cb(countries);
-  }, 500);
-};
-
-const requestForUsers = function (cb) {
-  const users = [
-    {
-      id: 1,
-      fistName: 'Chloe',
-      lastName: 'Snyder'
-    },
-    {
-      id: 44,
-      fistName: 'Valdemar',
-      lastName: 'Larsen'
-    },
-    {
-      id: 342,
-      fistName: 'Curtis',
-      lastName: 'Garza'
-    },
-    {
-      id: 2,
-      fistName: 'Sedef',
-      lastName: 'Sezek'
-    },
-    {
-      id: 24,
-      fistName: 'Emile',
-      lastName: 'Taylor'
-    }
-  ];
-
-  setTimeout(function () {
-    cb(users);
-  }, 500);
-};
-
-requestForUsers(function (users) {
-
-  requestForCountries(function (countries) {
-    const totalArr = users.map(user => {
-      const {country} = countries.find((currentValue) => currentValue.userId === user.id)
-
-      return {
-        ...user,
-        country
-      };
-    })
-
-    console.log(totalArr);
-    console.log(countries);
-    console.log(users);
-    printHtml(totalArr);
-  });
-});
+console.log(requestForUsers, requestForCountries)
 
 function createList() {
   return document.createElement('ul');
@@ -101,7 +23,13 @@ function printHtml(arr) {
 
     ul.appendChild(li);
   });
+  printHtml(arr);
   document.body.appendChild(ul);
 }
 
-export {requestForUsers, requestForCountries}
+
+
+
+
+
+
--- HW9/main2.js
+++ HW9/main2.js
@@ -1,6 +1,82 @@
-import {requestForUsers, requestForCountries} from './hw9.js';
-import * as test from './hw9.js';
+'use strict'
 
-console.log(requestForUsers)
-console.log(test.requestForUsers )
+const requestForCountries = function (cb) {
+  const countries = [
+    {
+      userId: 44,
+      country: 'Germany'
+    },
+    {
+      userId: 1,
+      country: 'Canada'
+    },
+    {
+      userId: 342,
+      country: 'Brazil'
+    },
+    {
+      userId: 24,
+      country: 'Denmark'
+    },
+    {
+      userId: 2,
+      country: 'Ireland'
+    }
+  ];
 
+  setTimeout(function () {
+    cb(countries);
+  }, 500);
+};
+
+const requestForUsers = function (cb) {
+  const users = [
+    {
+      id: 1,
+      fistName: 'Chloe',
+      lastName: 'Snyder'
+    },
+    {
+      id: 44,
+      fistName: 'Valdemar',
+      lastName: 'Larsen'
+    },
+    {
+      id: 342,
+      fistName: 'Curtis',
+      lastName: 'Garza'
+    },
+    {
+      id: 2,
+      fistName: 'Sedef',
+      lastName: 'Sezek'
+    },
+    {
+      id: 24,
+      fistName: 'Emile',
+      lastName: 'Taylor'
+    }
+  ];
+
+  setTimeout(function () {
+    cb(users);
+  }, 500);
+};
+
+requestForUsers(function (users) {
+
+  requestForCountries(function (countries) {
+    const totalArr = users.map(user => {
+      const {country} = countries.find((currentValue) => currentValue.userId === user.id)
+
+      return {
+        ...user,
+        country
+      };
+      console.log(totalArr);
+    })
+    console.log(users);
+    console.log(countries);
+  });
+});
+export {requestForUsers, requestForCountries}
