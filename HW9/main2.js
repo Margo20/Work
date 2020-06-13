5c8b1e63cd9113623009dbec25471bf3bd757d67
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-06-13 21:30:10 +0300

    success error

--- HW9/main2.js
+++ HW9/main2.js
@@ -1,6 +1,33 @@
 'use strict'
 
-const requestForCountries = function (cb) {
+const h1 = document.querySelector('h1');
+const isError = () => Math.floor(Math.random()*11) < 5;
+// const requestInfo = {
+//   total: 2,
+//   done: 0,
+//   isError: false
+// };
+// let users = [];
+// let countries = [];
+//
+// function registerRequest(isError) {
+//   requestInfo.done++;
+//
+//   if (isError) {
+//     requestInfo.isError = true;
+//   }
+//
+//   if (requestInfo.total === requestInfo.done){
+//     console.log('Done');
+//   }
+// }
+function printInfo() {
+  h1.innerText = 'Array are completely received';
+  // console.log(users);
+  // console.log(countries);
+}
+
+const requestForCountries = function (success, error) {
   const countries = [
     {
       userId: 44,
@@ -25,11 +52,19 @@ const requestForCountries = function (cb) {
   ];
 
   setTimeout(function () {
-    cb(countries);
+    if (isError()) {
+      error({
+        code: 500,
+        message: 'Internal server Error'
+      });
+    } else {
+        printInfo();
+        success(countries);
+      }
   }, 500);
 };
 
-const requestForUsers = function (cb) {
+const requestForUsers = function (fnSuccess, fnError) {
   const users = [
     {
       id: 1,
@@ -59,24 +94,75 @@ const requestForUsers = function (cb) {
   ];
 
   setTimeout(function () {
-    cb(users);
+    if (isError()) {
+      fnError({
+        code: 500,
+        message: 'Internal server Error'
+      });
+    } else {
+      fnSuccess(users);
+      printInfo();
+    }
   }, 500);
 };
 
+function errorHandler(error) {
+  h1.innerText = error.message;
+  console.log(error);
+}
+// function arrNew() {
+//   const totalArr = users.map(user => {
+//     const {country} = countries.find((currentValue) => currentValue.userId === user.id)
+//     return {
+//       ...user,
+//       country
+//     };
+//     console.log(users);
+//     console.log(countries);
+//     console.log(totalArr);
+//   })
+// }
+
+// requestForUsers(
+//   u => {
+//     users = [...u];
+//     registerRequest(false);
+//     arrNew();
+//   },
+//   err => {
+//     registerRequest(true);
+//     errorHandler();
+//   });
+//
+// requestForCountries(
+//   c => {
+//     countries = [...c];
+//     registerRequest(false);
+//     arrNew();
+//   },
+//   err => {
+//     registerRequest(true);
+//     errorHandler();
+//   });
+
+
 requestForUsers(function (users) {
-
   requestForCountries(function (countries) {
-    const totalArr = users.map(user => {
-      const {country} = countries.find((currentValue) => currentValue.userId === user.id)
 
-      return {
-        ...user,
-        country
-      };
-      console.log(totalArr);
-    })
-    console.log(users);
-    console.log(countries);
-  });
-});
+      const totalArr = users.map(user => {
+        const {country} = countries.find((currentValue) => currentValue.userId === user.id)
+        return {
+          ...user,
+          country
+        };
+        console.log(totalArr);
+      })
+      console.log(users);
+      console.log(countries);
+    }, errorHandler  )
+
+  }, errorHandler );
+
 export {requestForUsers, requestForCountries}
+
+
