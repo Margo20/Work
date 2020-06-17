872e1dddec6cd58830550cdab4f5fed11a8a324e
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-06-17 20:53:41 +0300

    hw9 new variant

--- HW9/hw9.js
+++ HW9/hw9.js
@@ -1,33 +1,29 @@
 'use strict'
-
+const isError = () => Math.floor(Math.random() * 10) < 0;
 import {requestForUsers, requestForCountries} from './main2.js';
 
-const isError = () => Math.floor(Math.random() * 10) < 0;
 const h1 = document.querySelector('h1');
-let users = [];
-let countries = [];
+const totalArr = [];
+let users;
+let countries;
 
-const requestInfo = {
-  total: 2,
-  done: 0,
-  isError: false
-};
-
-function registerRequest(isError) {
-  requestInfo.done++;
-
-  if (isError) {
-    requestInfo.isError = true;
-  }
-
-  if(requestInfo.total === requestInfo.done) {
-    printInfo(requestInfo.isError);
-  }
-};
-
-function printInfo() {
-  h1.innerText = 'Array are completely received';
-  }
+// const requestInfo = {
+//   total: 2,
+//   done: 0,
+//   isError: false
+// };
+//
+// function registerRequest(isError) {
+//   requestInfo.done++;
+//
+//   if (isError) {
+//     requestInfo.isError = true;
+//   }
+//
+//   if(requestInfo.total === requestInfo.done) {
+//     printInfo(requestInfo.isError);
+//   }
+// };
 
 function createList() {
   return document.createElement('ul');
@@ -50,64 +46,47 @@ function printHtml(arr) {
   });
   document.body.appendChild(ul);
 }
-let totalArr = [];
+function getSomething(newUsers, newCountries) {
+  if (newUsers !== undefined) {
+    users = newUsers;
+  }
+  if (newCountries !== undefined) {
+    countries = newCountries;
+  }
+
+  if (users !== undefined && countries !== undefined){
+    const totalArr = users.map(user => {
+      const {country} = countries.find((currentValue) => currentValue.userId === user.id)
+      return {
+        ...user,
+        country
+      };
+      console.log(totalArr);
+      printInfo();
+    })
+    printHtml(totalArr);
+  }
+}
 
 requestForUsers(function (users) {
-  for (let i=0; i<users.length; i++) {
-    let u = users[i];
-    let foundId = false;
+  getSomething(users, undefined);
 
-    for (let j=0; j<totalArr.length; j++) {
-      let total = totalArr[j];
-      if (total.id === u.id) {
-        total.fistName = u.fistName;
-        total.lastName = u.lastName;
-        foundId = true;
-        break;
-      }
-    }
-    if (!foundId) {
-      totalArr.push({
-        id: u.id,
-        lastName: u.lastName,
-        fistName: u.fistName
-      });
-    }
-  }
-  printInfo();
-  registerRequest(false);
+  // registerRequest(false);
 }, errorHandler );
 
 requestForCountries(function (countries) {
-  for (let i=0; i<countries.length; i++) {
-    let c = countries[i];
-    let foundId = false;
+  getSomething(undefined,countries );
 
-    for (let j=0; j<totalArr.length; j++) {
-      let total = totalArr[j];
-      if (total.id === c.userId) {
-        total.country = c.country;
-        foundId = true;
-        break;
-      }
-    }
-    if (!foundId) {
-      totalArr.push({
-        id: c.userId,
-        country: c.country
-      });
-    }
-  }
-  printInfo();
-  registerRequest(false);
+  // registerRequest(false);
 }, errorHandler );
-
-  console.log(totalArr);
-  printHtml(totalArr);
 
 function errorHandler(error) {
   h1.innerText = error.message;
   console.log(error);
+}
+
+function printInfo() {
+  h1.innerText = 'Array are completely received';
 }
 
 
